d5f67b581b58f364c6a5b27d8b9f40211d42ccd2
Niraj Amalkanti <neji49@gmail.com>
2014-08-02 22:01:37 -0700

    Made code for b1 processing cleaner and removed excess positioning code. Also added mode logic

--- bloch/bloch.py
+++ bloch/bloch.py
@@ -3,6 +3,7 @@ import scipy as sp
 
 from bloch.bloch_simulator import bloch_c
 
+from bloch.bloch_processing import NUMBER
 from bloch.bloch_processing import process_gradient_argument, process_time_points, process_off_resonance_arguments
 from bloch.bloch_processing import process_positions, process_magnetization, reshape_matrices
 
@@ -39,18 +40,22 @@ def bloch(b1, gr, tp, t1, t2, df, dp, mode, mx=None, my=None, mz=None):
             mx,my,mz = NxP arrays of the resulting magnetization
                             components at each position and frequency.
     """
-    grx, gry, grz = process_gradient_argument(gr)
-    tp = process_time_points(tp, b1.size)
+    if isinstance(b1, NUMBER):
+        b1 = np.ones(1) * b1
+    ntime = b1.size
+
+    grx, gry, grz = process_gradient_argument(gr, ntime)
+    tp = process_time_points(tp, ntime)
     df, nf = process_off_resonance_arguments(df)
     dx, dy, dz, n_pos = process_positions(dp)
-    pos_size = 1
-    if isinstance(dp, np.ndarray):
-        pos_size = dp.shape[0]
-    mx, my, mz = process_magnetization(mx, my, mz, b1.size, nf*pos_size, mode)
-    if 2 == mode:
-        ntout = b1.size
+    mx, my, mz = process_magnetization(mx, my, mz, ntime, nf*n_pos, mode)
+
+    if (2 & mode):
+        ntout = ntime
     else:
         ntout = 1
-    bloch_c(b1.real, b1.imag, grx, gry, grz, tp, b1.size, t1, t2, df, nf, dx, dy, dz, n_pos, mode, mx, my, mz)
+
+    bloch_c(b1.real, b1.imag, grx, gry, grz, tp, ntime, t1, t2, df, nf, dx, dy, dz, n_pos, mode, mx, my, mz)
+
     reshape_matrices(mx, my, mz, ntout, n_pos, nf)
     return mx, my, mz

d050fcc6e3f29525772b7adb2d7783999925494b
Niraj Amalkanti <neji49@gmail.com>
2014-08-02 22:00:37 -0700

    Shifted documention

--- bloch/bloch.py
+++ bloch/bloch.py
@@ -9,36 +9,35 @@ from bloch.bloch_processing import process_positions, process_magnetization, res
 def bloch(b1, gr, tp, t1, t2, df, dp, mode, mx=None, my=None, mz=None):
     """
     Bloch simulation of rotations due to B1, gradient and
-	off-resonance, including relaxation effects.  At each time
-	point, the rotation matrix and decay matrix are calculated.
-	Simulation can simulate the steady-state if the sequence
-	is applied repeatedly, or the magnetization starting at m0.
+    off-resonance, including relaxation effects.  At each time
+    point, the rotation matrix and decay matrix are calculated.
+    Simulation can simulate the steady-state if the sequence
+    is applied repeatedly, or the magnetization starting at m0.
 
-	INPUT:
-		b1 = (Mx1) RF pulse in G.  Can be complex.
-		gr = (Mx1,2,or 3) 1,2 or 3-dimensional gradient in G/cm.
-		tp = (Mx1) time duration of each b1 and gr point, in seconds,
-				or 1x1 time step if constant for all points
-				or monotonically INCREASING endtime of each
-				interval..
-                ntime = Number of time points
-		t1 = T1 relaxation time in seconds.
-		t2 = T2 relaxation time in seconds.
-		df = (Nx1) Array of off-resonance frequencies (Hz)
-		dp = (Px1,2,or 3) Array of spatial positions (cm).  
-			Width should match width of gr.
-		mode= Bitmask mode:
-			Bit 0:  0-Simulate from start or M0, 1-Steady State
-			Bit 1:  1-Record m at time points.  0-just end time.
+    INPUT:
+            b1 = (1xM) RF pulse in G.  Can be complex.
+            gr = ((1,2,or 3)xM) 1,2 or 3-dimensional gradient in G/cm.
+            tp = (1xM) time duration of each b1 and gr point, in seconds,
+                            or 1x1 time step if constant for all points
+                            or monotonically INCREASING endtime of each
+                            interval..
+            t1 = T1 relaxation time in seconds.
+            t2 = T2 relaxation time in seconds.
+            df = (1xN) Array of off-resonance frequencies (Hz)
+            dp = ((1,2,or 3)xP) Array of spatial positions (cm).  
+                    Width should match width of gr.
+            mode= Bitmask mode:
+                    Bit 0:  0-Simulate from start or M0, 1-Steady State
+                    Bit 1:  1-Record m at time points.  0-just end time.
 
-	(optional)
-		mx,my,mz (PxN) arrays of starting magnetization, where N
-			is the number of frequencies and P is the number
-			of spatial positions.
+    (optional)
+            mx,my,mz (NxP) arrays of starting magnetization, where N
+                    is the number of frequencies and P is the number
+                    of spatial positions.
 
-	OUTPUT:
-		mx,my,mz = PxN arrays of the resulting magnetization
-				components at each position and frequency.
+    OUTPUT:
+            mx,my,mz = NxP arrays of the resulting magnetization
+                            components at each position and frequency.
     """
     grx, gry, grz = process_gradient_argument(gr)
     tp = process_time_points(tp, b1.size)

333b7d5c1df5c3f75135c1f5199f9a7f2f731b1e
Niraj Amalkanti <neji49@gmail.com>
2014-08-10 00:55:22 -0700

    Added new test for sinc pulse

--- RFSeqTests.py
+++ RFSeqTests.py
@@ -3,7 +3,12 @@ import unittest
 import numpy as np
 import scipy as sp
 
-from bloch.rf_seq import hard_pulses
+from bloch.bloch import bloch
+from bloch.rf_seq import hard_pulses, sinc_pulse
+
+from BlochTest import get_data_with_key
+
+TEST_DIR = "test_data"
 
 class RFSeqTests(unittest.TestCase):
     """
@@ -29,6 +34,30 @@ class RFSeqTests(unittest.TestCase):
         flip_angle = np.ones(2) * np.pi / 2
         spin_echo_pulses = hard_pulses(b1, flip_angle, 20e-3, 100e-3, 2, 4e-6)
         self.assertTrue(np.allclose(expected_pulses, spin_echo_pulses))
+
+    def test_sinc_pulse(self):
+        """
+        Tests generation of sinc pulse.
+        """
+        expected_mxy = get_data_with_key(TEST_DIR, "rf_test", "mxy")
+
+        duration = 3.2e-3
+        dt = duration / 256
+        flip = np.pi / 2
+        tbw = 8
+
+        rf = sinc_pulse(tbw, flip, duration, dt)
+        g = np.ones(rf.size) * .6
+        dp = np.linspace(-2, 2, 512)
+        mx0 = np.zeros(512)
+        my0 = np.zeros(512)
+        mz0 = np.ones(512)
+
+        mx, my, mz = bloch(rf, g, dt, 100, 100, 0, dp, 0, mx0, my0, mz0)
+        mxy = mx + 1.0j*my
+        np.allclose(expected_mxy, mxy)
+
+
 
 if __name__ == "__main__":
     unittest.main()

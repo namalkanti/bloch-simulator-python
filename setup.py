63bd833b21d5e9b75f138d56d0da891084a435ef
Niraj Amalkanti <neji49@gmail.com>
2014-08-10 20:16:02 -0700

    Moved setup.py to upper level directory and modified to support installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+import numpy.distutils.misc_util
+
+setup(
+        name = "Bloch Simulator Library",
+        version = "1.0",
+        description = "Bloch Simulator and helper modules. Originally written by Brian Hargreaves and Mikki Lustig in Matlab.",
+        author = "Niraj Amalkant",
+        author_email = "neji49@gmail.com",
+        url = "https://github.com/neji49/bloch-simulator-python",
+        packages = ["bloch"],
+        ext_modules=[Extension("bloch.bloch_simulator", ["bloch/bloch_simulator.c"])],
+        include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
+        )
+

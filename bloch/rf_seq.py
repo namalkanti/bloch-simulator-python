ef06486123493bbcb5dbcb28a04dd7e99f4b5688
neji49 <neji49@gmail.com>
2019-06-16 18:15:58 -0400

    Coercing samples variable to integer to prevent automatic coercion

--- bloch/rf_seq.py
+++ bloch/rf_seq.py
@@ -42,7 +42,7 @@ def sinc_pulse(timebandwidth, flip_angle, duration, dt, gamma=26747.52):
     Output:
     rf: Sinc pulse rf signal
     """
-    samples = duration / dt
+    samples = int(duration / dt)
     theta = np.linspace(-timebandwidth/2, timebandwidth/2, samples+2)
     rf = np.sinc(theta[1:-1] * sig.hann(samples))
     rf = flip_angle * (rf/np.sum(rf))
